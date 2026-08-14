88077f4672f126eeca1a9f97aee5acfa7d1f49ab
codingingene <avoys.works20@gmail.com>
2026-08-14 11:14:17 +0530

    Changing card ui, adding pagination

--- core/views.py
+++ core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from core.models import Portfolio, PortfolioFile
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
     portfolios = Portfolio.objects.all()
 
+    pagination = Paginator(portfolios, per_page=1)
+    page = pagination.get_page(request.GET.get("page"))
+    page_count = pagination.num_pages
+
     data = {
-        "portfolios":portfolios,
+        "portfolio_page":page,
+        "page_count":page_count,
+        "page_count_iterator":[i for i in range(1, page_count+1)],
     }
 
     return render(request, "home.html", data)
